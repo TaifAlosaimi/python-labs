349dc8abd8f9c09f88e90bed616cdc17ec5d5fc8
Taif F <taif7falibrahim@gmail.com>
2026-08-06 04:35:21 +0300

    updated

---
+++ week- 2/Day 4/LAB5.py
@@ -0,0 +1,11 @@
+account_active = True
+has_permission = False
+
+if account_active:
+    if has_permission:
+        print("Access Granted")
+    else:
+        print("Access denied")
+
+else:
+    print("account is not active")
---
+++ week- 2/Day 4/LAB6.py
@@ -0,0 +1,11 @@
+name = "princess Taif"
+cart = []
+balance = 20
+
+if name:
+    print("name has a value")
+
+if not cart:
+    print("your cart is empty, please shop")
+
+print(bool(balance))
